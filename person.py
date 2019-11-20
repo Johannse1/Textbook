93a676eb14127f87a6f42449106511bf5733b0fc
Evan <johannse@hartwick.edu>
2019-11-20 09:37:43 -0500

    update

---
+++ person.py
@@ -0,0 +1,8 @@
+class Person():
+    def __init__(self, first_name, last_name, age):
+        self.first = first_name
+        self.last = last_name
+        self.age = age
+
+        def
+
---
+++ textbook.py
@@ -0,0 +1,11 @@
+
+class Book():
+    def __init__(self,title,author,edition,ISBN_number,publisher,year_published,quantity,price):
+        self.title = title
+        self.author = Person()
+        self.edition = edition
+        self.nuber = ISBN_number
+        self.publisher = publisher
+        self.year = year_published
+        self.quantity = quantity
+        self.price = price
